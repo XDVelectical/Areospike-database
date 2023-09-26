c46aae1f8dfb4d60ba79f26742fbd88a0245b410
Velectical <106909222+XDVelectical@users.noreply.github.com>
2023-09-26 10:36:56 +0200

    Create Database.lua

---
+++ src/Database.lua
@@ -0,0 +1,49 @@
+	-- Import the Aerospike module
+local aerospike = require("aerospike")
+ 
+-- Function to create an Aerospike database
+-- @param namespace: The namespace for the database.
+-- @param set: The set for the database.
+-- @param host: The host address of the Aerospike server.
+-- @param port: The port number of the Aerospike server.
+-- @return: Returns true if the database is created successfully, otherwise returns false.
+function createAerospikeDatabase(namespace, set, host, port)
+    -- Create a client object to connect to the Aerospike server
+    local client = aerospike.client({
+        hosts = {{ host = host, port = port }}
+    })
+ 
+    -- Connect to the Aerospike server
+    local status = client:connect()
+    if status == aerospike.OK then
+        -- Create a new database with the specified namespace and set
+        local createStatus = client:create_index(namespace, set)
+        if createStatus == aerospike.OK then
+            -- Database created successfully
+            client:close()
+            return true
+        else
+            -- Failed to create database
+            client:close()
+            return false
+        end
+    else
+        -- Failed to connect to the Aerospike server
+        return false
+    end
+end
+ 
+-- Example usage of the createAerospikeDatabase function
+ 
+-- Usage Example: Create an Aerospike database with namespace "test" and set "data"
+local namespace = "test"
+local set = "data"
+local host = "127.0.0.1"
+local port = 3000
+ 
+local success = createAerospikeDatabase(namespace, set, host, port)
+if success then
+    print("Aerospike database created successfully.")
+else
+    print("Failed to create Aerospike database.")
+end
